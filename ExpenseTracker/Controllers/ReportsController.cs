308513f9827a5ab760634c5df9b7f041ae43c41c
anilissac <12078999+anilissac@users.noreply.github.com>
2023-11-06 10:25:45 +0530

    changes

---
+++ ExpenseTracker/Controllers/ReportsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
